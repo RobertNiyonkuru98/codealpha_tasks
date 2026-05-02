d53a2a15a01b19c23388be10aa968577d8d447fc
Robert <r.niyonkuru@alustudent.com>
2026-05-02 16:43:49 +0200

    modify: changed the code to read html files for status display

--- CodeAlpha_URLShortner/server.js
+++ CodeAlpha_URLShortner/server.js
@@ -1,8 +1,27 @@
 const http = require("http")
 const fs = require('fs')
-const PORT = 3000
+const PORT = 4000
 
 const server = http.createServer(function(req, res){
+    fs.readFile("index.html", function(error, data) {
+        if (error){
+            fs.readFile("404.html", function(error, data){
+                if (error){
+                    res.writeHead(500)
+                    res.end("Server Error!")
+                } else {
+                    res.writeHead(404, {'Content-Type': 'text/html'})
+                    res.write(data)
+                    res.end()
+                }
+            })
+        }
+        else{
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(data)
+            res.end()
+        }
+    })
 
 })
 
