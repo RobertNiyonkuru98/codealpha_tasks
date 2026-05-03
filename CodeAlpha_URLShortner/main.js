a1687ec602e611f41bc87aa5c2449125f9dd755c
Robert <r.niyonkuru@alustudent.com>
2026-05-03 18:21:20 +0200

    feat: Set up schema and route logic for shortUrls

--- CodeAlpha_URLShortner/main.js
+++ CodeAlpha_URLShortner/main.js
@@ -3,11 +3,33 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+const ShortUrl = require('./models/shortUrl')
+
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database!'))
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+    const shortUrl = await ShortUrl.find()
+    res.render('index', { shortUrl: shortUrl })
+})
+
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
+    res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+
+    shortUrl.clicks++
+    await shortUrl.save()
+    res.redirect(shortUrl.full)
 })
 
 app.listen(PORT, () => console.log('Server is started on Port ' + PORT))
